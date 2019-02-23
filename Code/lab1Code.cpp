99c1be4e3c2739ae0c3219df984d45ef138a2594
Boikanyo13 <bv.radiokana@gmail.com>
2019-02-23 10:42:14 +0200

    Random generation of 2D matrix

--- Code/lab1Code.cpp
+++ Code/lab1Code.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -7,23 +9,42 @@ using vector1D = vector<double>;
 using vector2D =  vector<vector1D>;
 using vector3D = vector<vector2D>;
 
+//generate random 2D matrix with numbers between 0 and 20
+vector2D gen2Dmatrix(int n){
+ 
+ vector1D temp(n,0);
+ vector2D matrix(n,temp);
+ auto num = 0;
+ 
+ for (auto i = 0; i< n; i++){ 
+    for (auto j = 0 ; j< n ; j++){
+	   	
+           num = rand()%20;     
+           matrix[i][j] = num;
+     }
+  }
+  
+ return matrix;
 
+}
+
+//2D matrix multiplication
 vector2D rank2TensorMult(vector2D A, vector2D B){
 
  
  auto n = A.size();
  auto C = vector2D{};
 
- for(int i = 0; i < n; i++){
+ for(auto i = 0; i < n; i++){
     
   
     auto tempVector = vector<double>{};
 
-    for(int j = 0; j < n; j++){
+    for(auto j = 0; j < n; j++){
            
 	    auto tempSum =0;
     
-	    for(int k = 0; k < n; k++){
+	    for(auto k = 0; k < n; k++){
         	
 		    tempSum += (A[i]).at(k)*(B[k]).at(j);
 
@@ -39,43 +60,46 @@ return C;
 }
 
 
-//For Validating
+//Display matrix
 
 void display(vector2D x){
 
 	auto n = x.size();
 
-	for(int i = 0; i <n; i ++){
+	for(auto i = 0; i <n; i ++){
 	
-		for(int j = 0; j <n; j++){
+		for(auto j = 0; j <n; j++){
 		
 		cout << x[i][j] << " | ";
 		}
 	     cout << endl;
 	}
 
-
+	cout << endl;
 }
 
 
 int main(){
 
+        srand(time(0));
+	auto n = 0;
+        cout << "Please input the size(n) of the matrix: ";
+        cin>> n;
+        cout << endl;
 
-	auto A = vector2D{ vector1D{2,-5,1},
-		           vector1D{-3,6,2},
-		           vector1D{1,0,-1}};
-				
-        auto B = vector2D{vector1D{0,-1,1},
-			  vector1D{-2,2,2},
-	                  vector1D{4,5,-1}};
+        auto A = gen2Dmatrix(n);
+        auto B = gen2Dmatrix(n);
+
+        cout << " Matrix A =  "<< endl;
         display(A);
 
-	cout << " X "<< endl;
+	cout << " Matrix B =  "<< endl;
 	display(B);
-	cout<< " = " << endl;
+	
         
 	auto C = rank2TensorMult(A,B);
 
+        cout <<" Matrix C =  "<< endl;
         display(C);
 
 return 0;}
