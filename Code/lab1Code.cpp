0b40a0fec352dd56d1da508aa586a531e4483999
Boikanyo13 <bv.radiokana@gmail.com>
2019-02-24 00:09:23 +0200

    Addition of computation functions to clean up main

--- Code/lab1Code.cpp
+++ Code/lab1Code.cpp
@@ -10,73 +10,76 @@ using vector2D =  vector<vector1D>;
 using vector3D = vector<vector2D>;
 
 //generate random 2D matrix with numbers between 0 and 20
+
 vector2D gen2Dmatrix(int n){
  
- vector1D temp(n,0);
- vector2D matrix(n,temp);
- auto num = 0;
+   vector1D temp(n,0);
+   vector2D matrix(n,temp);
+   auto num = 0;
  
- for (auto i = 0; i< n; i++){ 
-    for (auto j = 0 ; j< n ; j++){
+   for (auto i = 0; i< n; i++){ 
+      for (auto j = 0 ; j< n ; j++){
 	   	
            num = rand()%20;     
            matrix[i][j] = num;
-     }
-  }
+      }
+   }
   
- return matrix;
+   return matrix;
 
 }
 
 //2D matrix addition
+
 vector2D rank2TensorAdd(int n,vector2D A, vector2D B){
  
- vector1D temp(n,0);
- vector2D C(n,temp);
+   vector1D temp(n,0);
+   vector2D C(n,temp);
  
- for (auto i = 0;i < n; i++){
-     for(auto j = 0; j<n; j++){
-	C[i][j] = A[i][j]+B[i][j];
-      }
- }
+   for (auto i = 0;i < n; i++){
+       for(auto j = 0; j<n; j++){
+
+	   C[i][j] = A[i][j]+B[i][j];
+        }
+    }
  
- return C;
+   return C;
 
 }
 
 
 //2D matrix multiplication
+
 vector2D rank2TensorMult(vector2D A, vector2D B){
 
- 
- auto n = A.size();
- auto C = vector2D{};
+   auto n = A.size();
+   auto C = vector2D{};
 
- for(auto i = 0; i < n; i++){
+   for(auto i = 0; i < n; i++){
     
-  
-    auto tempVector = vector<double>{};
+      auto tempVector = vector<double>{};
 
-    for(auto j = 0; j < n; j++){
+           for(auto j = 0; j < n; j++){
            
-	    auto tempSum =0;
+	       auto tempSum =0;
     
-	    for(auto k = 0; k < n; k++){
+	           for(auto k = 0; k < n; k++){
         	
-		    tempSum += (A[i]).at(k)*(B[k]).at(j);
+		         tempSum += (A[i]).at(k)*(B[k]).at(j);
 
-	    }
+	           }
 
-	  tempVector.push_back(tempSum);
-     } 
+	       tempVector.push_back(tempSum);
+            } 
     C.push_back(tempVector);
  }
 
 
-  return C;
+   return C;
 }
 
 //generate random 3D matrix with numbers between 0 and 20
+
 vector3D gen3Dmatrix(int n){
 
   vector3D matrix;
@@ -91,150 +94,143 @@ vector3D gen3Dmatrix(int n){
 }
 
 
-//Display matrix
+//Print 2D matrix
 
 void print(vector2D x){
 
-	auto n = x.size();
+   auto n = x.size();
 
-	for(auto i = 0; i <n; i ++){
+   for(auto i = 0; i <n; i ++){
 	
-		for(auto j = 0; j <n; j++){
+	for(auto j = 0; j <n; j++){
 		
-		cout << x[i][j] << " | ";
-		}
-	     cout << endl;
+	     cout << x[i][j] << " | ";
 	}
-
 	cout << endl;
+    }
+
+    cout << endl;
 }
 
-//3D MULTIPLICATION
+//3D Multiplication
 
-vector3D rank3TensorMult(vector3D a, vector3D b)
-{
-	        auto  n = a.size();
-		auto C = vector3D{};
+vector3D rank3TensorMult(vector3D a, vector3D b){
 
-		for(auto i = 0; i < n; i++){
+   auto  n = a.size();
+   auto C = vector3D{};
+
+   for(auto i = 0; i < n; i++){
 		
-		       auto subC = vector2D{};
+       auto subC = vector2D{};
                        
-                       subC = rank2TensorMult(a.at(i),b.at(i));
+       subC = rank2TensorMult(a.at(i),b.at(i));
 		       
-		       C.push_back(subC);
+       C.push_back(subC);
                                
-			}
+    }
 		
-		return C;
+   return C;
 		 
 }
 
-//3D ADDITION
+//3D Addition
 
 vector3D rank3TensorAdd(int n,vector3D a, vector3D b)
 {
 	
-		auto C = vector3D{};
+   auto C = vector3D{};
 
-		for(auto i = 0; i < n; i++){
+   for(auto i = 0; i < n; i++){
 		
-		       auto subC = vector2D{};
+        auto subC = vector2D{};
                        
-                       subC = rank2TensorAdd(n,a.at(i),b.at(i));
+        subC = rank2TensorAdd(n,a.at(i),b.at(i));
 		       
-		       C.push_back(subC);
+        C.push_back(subC);
                                
-			}
-	return C;
+    }
+
+   return C;
 }
 
+//Print 3D matrix
 
 void print(vector3D x){
 
    auto n = x.size();
       
-       	  cout << "===========\n" << endl;
-       
-	for(int i = 0; i < n; i++){
+   for(int i = 0; i < n; i++){
 
-            print(x[i]);
+       print(x[i]);
 
-          	 }
-
-	  cout << "===========\n" << endl;
+    }
+   
+   cout << endl;
 }
 
+//Computations and display
 
+void _2D_computations(int n){
 
+   auto A = gen2Dmatrix(n);
+   auto B = gen2Dmatrix(n);
+   cout << "============2D Computations============" <<"\n" <<endl;
+   cout << " Matrix A =  "<< endl;
+   print(A);
 
+   cout << " Matrix B =  "<< endl;
+   print(B);
+   
+   cout << " ADDITION (A + B)" << endl;
+   cout <<"  Matrix C =  "<< endl;
+ 
+   auto C = rank2TensorAdd(n,A,B);
+   auto C_1 = rank2TensorMult(A,B);
+   print(C);
+   cout << " MULTIPLICATION (A X B)" << endl;
+   
+   cout <<" Matrix C =  "<< endl;
+   print(C_1);
+
+}
+
+void _3D_computations(int n){
+
+   auto A = gen3Dmatrix(n);
+   auto B = gen3Dmatrix(n);
+  
+   cout << "============3D Computations============" <<"\n" <<endl;
+   cout << " Matrix A =  "<< endl;
+   print(A);
+
+   cout << " Matrix B =  "<< endl;
+   print(B);
+   
+   cout << " ADDITION (A + B)" << endl;
+   cout <<"  Matrix C =  "<< endl;
+  
+   auto C = rank3TensorAdd(n,A,B);
+   auto C_1 = rank3TensorMult(A,B);
+   print(C);
+
+   cout << " MULTIPLICATION (A X B)" << endl;
+   cout <<" Matrix C =  "<< endl;
+   print(C_1);
+  
+}
 
 int main(){
 
-        srand(time(0));
-	auto n = 0;
-        cout << "Please input the size(n) of the matrix: ";
-        cin>> n;
-        cout << endl;
+   srand(time(0));
+   auto n = 0;
+   cout << "Please input the size(n) of the matrix: ";
+   cin>> n;
+   cout << endl;
            
-        /*auto A = gen2Dmatrix(n);
-        auto B = gen2Dmatrix(n);
-		auto C_one = gen2Dmatrix(n);
-
-        cout << " Matrix A =  "<< endl;
-        display(A);
-
-	cout << " Matrix B =  "<< endl;
-	display(B);
-	
-		cout << " Matrix C_one =  "<< endl;
-	display(C_one);
-
-	auto C = rank2TensorMult(A,B);
-	auto C_1 = rank2TensorAdd(n,A,B);
-        	
-        cout << "2D MULTIPLICATION of A X B" << endl;
-		cout <<" Matrix C =  "<< endl;
-        display(C);
-
-        cout << " ADDITION Result of A + B" << endl;
-		cout <<" Matrix C_1 =  "<< endl;
-        display(C_1);
-			cout <<endl;
-			cout <<endl;
-	*/
+   _2D_computations(n);
+   _3D_computations(n);
 
 
-	/*auto A = vector2D{ vector1D{2,5,1},
-		           vector1D{3,6,2},
-		           vector1D{1,0,1}};
-				
-        auto B = vector2D{vector1D{0,1,1},
-			  vector1D{2,2,2},
-	                  vector1D{4,5,1}};
-      
-       	
-	auto A3 = vector3D{A,B,A};
-       	auto B3 = vector3D{B,A,B};*/
-        
-        auto A3 = gen3Dmatrix(n);
-        auto B3 = gen3Dmatrix(n);
-
-        print(A3);
-        cout << " x \n\n\n ";
-        print(B3);
-
-        //auto C3 = rank3TensorAdd(n,A3,B3);
-        //cout << "  =  " << "\n\n";
-       	//print(C3);
-      
-        auto C3_ = rank3TensorMult(A3,B3);
-        cout << "  =  " << "\n\n";
-       	print(C3_);
-        
-        
-       
-
-
-return 0;}
+    return 0;
+}
 
